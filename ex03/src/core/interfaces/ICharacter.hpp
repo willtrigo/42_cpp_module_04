2fa532da8ded76dda36c4e90c8b20ba94a845bec
willtrigo <willtrigo@gmail.com>
2025-03-24 22:32:16 -0300

    Feat: add ICharacter.hpp  Changes to be committed: 	modified:   src/core/interfaces/ICharacter.hpp

--- ex03/src/core/interfaces/ICharacter.hpp
+++ ex03/src/core/interfaces/ICharacter.hpp
@@ -6,7 +6,24 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 22:14:59 by dande-je          #+#    #+#             */
-/*   Updated: 2025/03/24 22:15:00 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/03/24 22:30:00 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ICHARACTER_HPP
+#define ICHARACTER_HPP
+
+#include <string>
+
+class AMateria;
+
+class ICharacter {
+ public:
+  virtual ~ICharacter() {}
+  virtual std::string const & getName() const = 0;
+  virtual void equip(AMateria* m) = 0;
+  virtual void unequip(int idx) = 0;
+  virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif // ICHARACTER_HPP
