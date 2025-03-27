78da153611f40546fd197b523d08f0b96aa85be7
willtrigo <willtrigo@gmail.com>
2025-03-27 12:00:47 -0300

    Feat: implement main as pdf required  Changes to be committed: 	modified:   src/main/main.cpp

--- ex03/src/main/main.cpp
+++ ex03/src/main/main.cpp
@@ -6,12 +6,39 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 21:50:19 by dande-je          #+#    #+#             */
-/*   Updated: 2025/03/24 22:07:35 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/03/27 11:53:32 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "core/entities/Character.hpp"
+#include "core/interfaces/ICharacter.hpp"
+#include "core/interfaces/IMateriaSource.hpp"
+#include "core/materials/Cure.hpp"
+#include "core/materials/Ice.hpp"
+#include "core/services/MateriaSource.hpp"
 #include <cstdlib>
 
 int main() {
+  {
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+
+    ICharacter* me = new Character("me");
+    AMateria* tmp;
+
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+
+    delete bob;
+    delete me;
+    delete src;
+  }
   return EXIT_SUCCESS;
 }
